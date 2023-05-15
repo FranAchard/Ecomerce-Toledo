4df5387be767073343d1f11a336e07a5379a2a44
FranAchard <franachard8@gmail.com>
2023-05-15 19:45:27 -0300

    Auth0

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
-import './App.css'
-
+import './App.css';
+import Home from './components/Home/Home';
+import {Routes, Route} from 'react-router-dom'
 function App() {
 
   return (
     <>
-      Nico puto
+      <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes>
     </>
   )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,10 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <Router>
-    <App />
+    <Auth0Provider
+      domain="dev-5oisd5che2fwlmjr.us.auth0.com"
+      clientId="fgMh6snjBZrU7plF1lyLslo85xHXOeIE"
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+      <App />
+    </Auth0Provider>,
   </Router>,
   document.getElementById('root')
 );
