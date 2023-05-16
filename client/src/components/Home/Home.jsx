584bb79216590e04d9b45077e4f3a6f4d6736545
FranAchard <franachard8@gmail.com>
2023-05-16 18:40:35 -0300

    Dark Mode

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Login from "../Login/Login";
+import DarkMode from "../DarkMode/DarkMode";
 const Home =()=>{
     return (
         <>
         <Login/>
+        <DarkMode/>
         </>
     )
 }
