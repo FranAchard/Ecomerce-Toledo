5a9ea60ecbfc912b6669ad5ccc4af7bff84aaa86
FranAchard <franachard8@gmail.com>
2023-05-17 19:37:07 -0300

    Posicionamiento navbar y carrousel

---
+++ client/src/components/Home/Carrousel/Carrousel.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./carrousel.scss"
+const Carrousel=()=>{
+    return(
+        <div className="container-carrousel">
+            <p>Carrousel</p>
+        </div>
+    )
+}
+export default Carrousel
